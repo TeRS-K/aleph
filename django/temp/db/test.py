0ce2bd06e24637b4d8f246107158849b08a84c73
Kang Qing Yun <User@Users-MacBook-Air.local>
2018-01-27 13:12:36 -0500

    django-V1

---
+++ django/temp/db/test.py
@@ -0,0 +1,15 @@
+from Connector import Connection
+
+conn = Connection("18.216.32.253", "root", "password", "test")
+
+
+values = ["'Felix'", "'V1'", "'Dead'"]
+
+conn.insert("people", values)
+
+
+
+# conn.delete("people", ['''name="Emily"'''])
+
+
+print(conn.query("people", "*"))
